752235cd836b01287c95685877fb6a6f1c2687d0
jon-hanson <jon.hanson@gmail.com>
2024-01-02 21:40:40 +0000

    Codec tests.

--- common/src/test/java/org/typemeta/arrowcache/common/QueryCodecTest.java
+++ common/src/test/java/org/typemeta/arrowcache/common/QueryCodecTest.java
@@ -3,14 +3,58 @@ package org.typemeta.arrowcache.common;
 import org.junit.jupiter.api.Test;
 import org.typemeta.arrowcache.common.avro.Query;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.*;
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class QueryCodecTest {
-    @Test
-    public void roundTripQueryApiToAvroCodec() {
-        final Query avroQuery = QueryApiToAvroCodec.INSTANCE.encode(TestData.QUERY);
-        final Api.Query query2 = QueryApiToAvroCodec.INSTANCE.decode(avroQuery);
+    private static final BiCodec<Api.Query, Consumer<OutputStream>, Api.Query, InputStream> API_TO_BYTES_CODEC =
+            QueryApiToAvroCodec.INSTANCE.andThen(QueryAvroToStreamCodec.INSTANCE);
 
-        assertEquals(TestData.QUERY, query2, "Round-tripped query");
+    @Test
+    public void roundTripQueryApiToAvro() {
+        final Query avroQuery = QueryApiToAvroCodec.INSTANCE.encode(TestData.API_QUERY);
+
+        assertEquals(TestData.AVRO_QUERY, avroQuery, "API query converted to Avro");
+
+        final Api.Query apiQuery2 = QueryApiToAvroCodec.INSTANCE.decode(avroQuery);
+
+        assertEquals(TestData.API_QUERY, apiQuery2, "API query round-tripped via Avro");
+    }
+
+    @Test
+    public void roundTripQueryAvroToApi() {
+        final Api.Query apiQuery = QueryApiToAvroCodec.INSTANCE.decode(TestData.AVRO_QUERY);
+
+        assertEquals(TestData.API_QUERY, apiQuery, "Avro query converted to API");
+
+        final Query avroQuery2 = QueryApiToAvroCodec.INSTANCE.encode(apiQuery);
+
+        assertEquals(TestData.AVRO_QUERY, avroQuery2, "Avro query round-tripped via API");
+    }
+
+    @Test
+    public void roundTripQueryAvroToBytes() {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        QueryAvroToStreamCodec.INSTANCE.encode(TestData.AVRO_QUERY).accept(baos);
+
+        final ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        final Query avroQuery2 = QueryAvroToStreamCodec.INSTANCE.decode(bais);
+
+        assertEquals(TestData.AVRO_QUERY, avroQuery2, "Avro query round-tripped via byte array");
+        assertTrue(bais.available() == 0, "Byte array stream has been completely consumed");
+    }
+
+    @Test
+    public void roundTripQueryApiToBytes() {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        API_TO_BYTES_CODEC.encode(TestData.API_QUERY).accept(baos);
+
+        final ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        final Api.Query apiQuery2 = API_TO_BYTES_CODEC.decode(bais);
+
+        assertEquals(TestData.API_QUERY, apiQuery2, "API query round-tripped via byte array");
+        assertTrue(bais.available() == 0, "Byte array stream has been completely consumed");
     }
 }
--- common/src/test/java/org/typemeta/arrowcache/common/TestData.java
+++ common/src/test/java/org/typemeta/arrowcache/common/TestData.java
@@ -1,23 +1,55 @@
 package org.typemeta.arrowcache.common;
 
+import org.typemeta.arrowcache.common.avro.*;
+
 import java.util.*;
 
 public class TestData {
-    public static final Api.Query QUERY;
+    public static final Api.Query API_QUERY;
+
+    private static final String ATTR_A = "name";
+    private static final String ATTR_B = "aliases";
+
+    private static final String VALUE_1 = "test";
+    private static final String VALUE_2 = "alpha";
+    private static final String VALUE_3 = "beta";
+    private static final String VALUE_4 = "gamma";
 
     static {
         final List<Api.Filter> filters = new ArrayList<>();
+
         filters.add(new Api.SVFilter(
-                "name",
+                ATTR_A,
                 Api.SVFilter.Operator.NOT_EQUALS,
-                "test"
+                VALUE_1
         ));
+
         filters.add(new Api.MVFilter(
-                "aliases",
+                ATTR_B,
                 Api.MVFilter.Operator.IN,
-                Set.of("alpha", "beta", "gamma")
+                new TreeSet<>(Set.of(VALUE_2, VALUE_3, VALUE_4))
         ));
-        QUERY = new Api.Query(filters);
+
+        API_QUERY = new Api.Query(filters);
     }
 
+    public static final Query AVRO_QUERY;
+
+    static {
+        AVRO_QUERY = Query.newBuilder()
+                .setFilters(
+                        List.of(
+                                SVFilter.newBuilder()
+                                        .setAttribute(ATTR_A)
+                                        .setOperator(SVOperator.NOT_EQUALS)
+                                        .setValue(VALUE_1)
+                                        .build(),
+                                MVFilter.newBuilder()
+                                        .setAttribute(ATTR_B)
+                                        .setOperator(MVOperator.IN)
+                                        .setValues(List.of(VALUE_2, VALUE_3, VALUE_4))
+                                        .build()
+                        )
+                ).build();
+    }
 }
